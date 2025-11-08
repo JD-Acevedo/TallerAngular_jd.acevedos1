ec8b3d2ba89ba3df2bb8dca3a5a812dac11fbb71
UsuarioGithubJD-Acevedo <jd.acevedos1@uniandes.edu.co>
2025-11-08 11:56:23 -0500

    titulo y creacion de modulo serie

---
+++ mynewapp/src/app/serie/serie-list/serie-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-serie-list',
+  standalone: false,
+  templateUrl: './serie-list.component.html',
+  styleUrl: './serie-list.component.css'
+})
+export class SerieListComponent {
+
+}
---
+++ mynewapp/src/app/serie/serie.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SerieListComponent } from './serie-list/serie-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    SerieListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SerieModule { }
